64317fb9ee367f18b7b524d28744e9f2dec8bd92
Manoj Paramsetti <paramsetti.manoj@gmmail.com>
2021-10-23 11:11:09 +0530

    Changes: update project cards

--- src/components/ProjectMainComponent.js
+++ src/components/ProjectMainComponent.js
@@ -5,19 +5,27 @@ function ProjectMain() {
     <section id="Projects">
         <section id="card">
             <div class="hovercard">
-                <img style={{height: "200px"}} src="https://manoj-paramsetti.github.io/img/britto.jpg" width="100%" alt="" srcSet="" />
+                <img style={{height: "200px"}} src="https://images.unsplash.com/photo-1508873699372-7aeab60b44ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" width="100%" alt="" srcSet="" />
                 <div class="bg"></div>
             </div>
-            <h1>Britto Shoe App</h1>
+            <h1>NotePad Clone</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
-                It is an e-commerce app to show the products. This project is just to display the products with cool animation. It cannot support the transaction part since it is for product promotional app
-                {'\n\n\n'}<b>Tech Stack:</b> Flutter 2.0 (Null Safety)
+                A simple windows notepad clone app created using C# with .Net framework. 
+                This has 90% features available in original notepad.{'\n'}
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Changes font style{'\n'}
+                2. Reads a file{'\n'}
+                3. Modifys a file
+                {'\n\n'}<b>Language(s) used:</b> C#
+                {'\n'}<b>Framework used:</b> .Net Framework 
+                {'\n'}<b>Platform:</b> Windows
             </p>
             {'\n\n'}
-            <inactive href="" target="_blank" rel="noopener noreferrer">
-                <p style={{textAlign: "end", paddingRight: "8px"}}>on Development</p>
-            </inactive>
+            <a href="https://github.com/Manoj-Paramsetti/M-Text-Editor-CSharp" target="_blank" rel="noopener noreferrer">
+                <p style={{textAlign: "end", paddingRight: "8px"}}>Code</p>
+            </a>
         </section>
+        
         <section id="card">
             <div class="hovercard">
                 <img style={{height: "200px"}} src="https://media.istockphoto.com/vectors/office-team-character-brainstorm-work-conference-business-people-at-vector-id1093700482?k=6&m=1093700482&s=612x612&w=0&h=UypPg3QwIT9odMhu1HFl5eQP25JkRgdMacTWHG_2JWA=" width="100%" alt="" srcSet="" />
@@ -25,8 +33,14 @@ function ProjectMain() {
             </div>
             <h1>Education Engage</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
-                A website to share the student's doubts in a FORUM, where professors and students can answer those questions and even a place to engage all students to discuss something related to academics
-                {'\n\n'}<b>Tech Stack:</b> ReactJS, NodeJS, ExpressJS, MongoDB and Firebase
+                A website to share student's doubts and academics related queries in a FORUM model.
+                Where professors and any students can reply or give answer for any questions
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. It can act as FORUM{'\n'}
+                2. It can add likes{'\n'}
+                3. It has CRUD operation
+                {'\n\n'}<b>Language(s) used:</b> Java Script
+                {'\n'}<b>Framework & Tools used:</b> ReactJS, Axios, NodeJS, ExpressJS, MongoDB and Firebase
             </p>
             {'\n'}
             <a href="https://educationengage.vercel.app/" target="_blank" rel="noopener noreferrer">
@@ -40,8 +54,14 @@ function ProjectMain() {
             </div>
             <h1>Personal Portfolio</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
-                The thing which you're currently using is my portfolio. I have used ReactJS to create my portfolio. You can check all my technical skills, education, and project works in my portfolio
-                {'\n\n\n'}<b>Tech Stack:</b> ReactJS
+                The thing which you're currently using is my portfolio. You can check all my technical skills, education, and project works in my portfolio
+                {'\n\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Showcases my perojects{'\n'}
+                2. List out my technical skills{'\n'}
+                3. Shares my contact deatails
+                {'\n\n'}<b>Language(s) used:</b> Java Script
+                {'\n'}<b>Framework used:</b> ReactJS
+                {'\n'}<b>Type:</b> Web Site
             </p>
             {'\n\n'}
             <a href="https://github.com/Manoj-Paramsetti/portfolio-react" target="_blank" rel="noopener noreferrer">
@@ -55,12 +75,17 @@ function ProjectMain() {
             </div>
             <h1>Covid Discord Bot</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
-                A covid tracker app to get covid cases from REST API to discord bot. In this app, It will suggest home treatment and precaution. It will show the covid symptoms to the user
-                {'\n\n\n'}
-                <b>Tech Stack:</b> Python3, Discord
+                A covid tracker bot to get covid cases from REST API to discord bot. In this app, It will suggest home treatment, precaution and shows the covid symptoms to the user
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Shows cases info for all countries{'\n'}
+                2. Insists to follow safety measures{'\n'}
+                3. Tells the symptoms of novel coronavirus
+                {'\n\n'}<b>Language(s) used:</b> Python3
+                {'\n'}<b>Libraries used:</b> Discord, http
+                {'\n'}<b>Type:</b> Bot
             </p>
             {'\n\n'}
-            <a href="http://github.com/Manoj-Paramsetti/Brutter-2" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Manoj-Paramsetti/corona-discord-bot" target="_blank" rel="noopener noreferrer">
                 <p style={{textAlign: "end", paddingRight: "8px"}}>Code</p>
             </a>
         </section>
@@ -72,12 +97,17 @@ function ProjectMain() {
             <h1>Covid Case Tracker App</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
                 A covid tracker app to get covid cases from REST API to flutter app. In this app, It will suggest home treatment and precaution. It will show the covid symptoms to the user
-                {'\n\n\n'}
-                <b>Tech Stack:</b> Flutter 2.0 (Null Safety)
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Shows cases info for all countries{'\n'}
+                2. Tells the symptoms of novel coronavirus{'\n'}
+                3. Shows corona vaccine center and vaccine availablilty
+                {'\n\n'}<b>Language(s) used:</b> Flutter 2.0
+                {'\n'}<b>Libraries used:</b> Material, http
+                {'\n'}<b>Platform:</b> Android, Web
             </p>
-            {'\n\n'}
-            <a href="http://github.com/Manoj-Paramsetti/Brutter-2" target="_blank" rel="noopener noreferrer">
-                <p style={{textAlign: "end", paddingRight: "8px"}}>Code</p>
+            {'\n'}
+            <a href="https://github.com/Manoj-Paramsetti/corona-tracker-flutter" target="_blank" rel="noopener noreferrer">
+                    <p style={{textAlign: "end", paddingRight: "8px"}}>Code</p>
             </a>
         </section>
 
@@ -89,8 +119,12 @@ function ProjectMain() {
             <h1>Password Generator</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
                 A Password-Generator CLI to create a strong password from the weak password. This password can be regenerated with the same password using the username and a weak password.
-                {'\n\n'}
-                <b>Tech Stack:</b> Python3
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Makes weak password into strong password{'\n'}
+                2. Password is retrived without DB{'\n'}    
+                {'\n\n'}<b>Language(s) used:</b> Python3
+                {'\n'}<b>Libraries used:</b> -
+                {'\n'}<b>Type:</b> CLI App
             </p>
             {'\n'}
             <a href="http://github.com/Manoj-Paramsetti/Password-Generator" target="_blank" rel="noopener noreferrer">
@@ -105,8 +139,12 @@ function ProjectMain() {
             <h1>Password Cracker</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
                 This app can create a wordlist with the custom word so that cracking the password is a bit easier. This tool can save the password either in a file or it will print the password list in the console
-                {'\n\n\n'}
-                <b>Tech Stack:</b> Python3
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Generates a password list with the given words{'\n'}
+                2. Saves wordlist in a file{'\n'}
+                {'\n'}<b>Language(s) used:</b> Python3
+                {'\n'}<b>Libraries used:</b> -
+                {'\n'}<b>Type:</b> CLI App
             </p>
             {'\n'}
             <a href="http://github.com/Manoj-Paramsetti/Brutter-2" target="_blank" rel="noopener noreferrer">
@@ -123,8 +161,12 @@ function ProjectMain() {
             <h1>Mobile Browser Lite</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
                 A small mobile browser. It will enable to use of all social media apps on full screen and It has many shortcuts to open some websites like a social media app, track flights, track cyclone.
-                {'\n\n'}
-                <b>Tech Stack:</b> Kotlin (Android)
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Works as browser{'\n'}
+                2. Quick access for some website{'\n'}
+                3. Full screen browser  
+                {'\n\n'}<b>Language(s) used:</b> Kotlin
+                {'\n'}<b>Platform:</b> Android
             </p>
             {'\n\n'}
             <a href="https://github.com/Manoj-Paramsetti/simple-web-browser-android" target="_blank" rel="noopener noreferrer">
@@ -141,8 +183,11 @@ function ProjectMain() {
             <h1>Hand Cricket Game</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
                 It is a mobile game to play hand cricket game. It's the favorite indoor games for kids studying in school. This app is created with an easy interface so that kids can use it easily.
-                {'\n\n\n'}
-                <b>Tech Stack:</b> Kotlin (Android)
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Computer game mode{'\n'}
+                2. Plays game{'\n\n'}  
+                {'\n\n'}<b>Language(s) used:</b> Kotlin
+                {'\n'}<b>Platform:</b> Android
             </p>
             {'\n'}
             <a href="https://github.com/Manoj-Paramsetti/Hand-Cricket-Android" target="_blank" rel="noopener noreferrer">
@@ -156,8 +201,13 @@ function ProjectMain() {
             </div>
             <h1>Morse Code Converter</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
-                A CLI tool to encrypt and decrypt morse code language. It's will convert simple text to morse code and morse code to simple text. It's independent to external library.{'\n\n\n'}
-                <b>Tech Stack:</b> Python3
+                A CLI tool to encrypt and decrypt morse code language. It's will convert simple text to morse code and morse code to simple text. It's independent to external library.
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Converts normal text to morse code{'\n'}
+                2. Converts morse code to normal text{'\n'}
+                {'\n'}<b>Language(s) used:</b> Python3
+                {'\n'}<b>Libraries used:</b> -
+                {'\n'}<b>Type:</b> CLI App
             </p>
             {'\n'}
             <a href="https://github.com/Manoj-Paramsetti/ASCII-MorseCode" target="_blank" rel="noopener noreferrer">
@@ -172,7 +222,13 @@ function ProjectMain() {
             <h1>Tic Tac Toe Game</h1>
             <p style={{fontSize: "14px", textAlign: "justify", padding: "5px", opacity: "0.8"}}>
                 I have created a small website to play Tic Tac Toe with the computer. The position of the boxes is chosen randomly for the computer to mark the place with O.
-                {'\n\n\n'}<b>Tech Stack:</b> Javascript, HTML and CSS
+                {'\n\n'}<b>Things it can do:</b>{'\n'}
+                1. Converts normal text to morse code{'\n'}
+                2. Converts morse code to normal text{'\n'}
+                {'\n'}<b>Language(s) used:</b> Javascript
+                {'\n'}<b>Libraries used:</b> -
+                {'\n'}<b>Type:</b> CLI App
+
             </p>
             {'\n'}
             <a href="https://manoj-paramsetti.github.io/tic-tac-toe/" target="_blank" rel="noopener noreferrer">
